8a2c360b419f87523874cf951a1160c602042e47
Blaine Lawson <blaine.lawson@gmail.com>
2019-06-03 14:12:35 +0000

    Automatically backed up by Learn

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -2,8 +2,12 @@ class TweetsController < ApplicationController
   get '/tweets' do
 
     # binding.pry
+    if logged_in?
       @tweets = Tweet.all
       erb :'/tweets/tweets'
+    else
+      redirect '/login'
+    end
   end
 
   get '/tweets/new' do
