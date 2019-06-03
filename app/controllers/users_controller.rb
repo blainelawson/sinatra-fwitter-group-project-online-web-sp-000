5d509d681fd03535c0cff4f95bcf58e891a76840
Blaine Lawson <blaine.lawson@gmail.com>
2019-06-03 14:46:31 +0000

    Automatically backed up by Learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
 
   get '/users/:slug' do
-    binding.pry
+    # binding.pry
     @user = User.find_by(username: params[:slug])
     erb :'/users/show'
   end
